3f98c9b727e1dc01c1583b824914bfcbe1a71c43
sarwar-asik <shasik2021@gmail.com>
2023-03-10 23:45:06 +0600

    Promises handle

--- Promises.js
+++ Promises.js
@@ -1,1 +1,33 @@
 console.log("---------Promises.js --------");
+
+const myPromise = new Promise((resolved, reject) => {
+  const user = { name: "Dinku", age: 111 };
+  //   const user = null;
+  if (!user) {
+    reject("Rejected hoise");
+  } else {
+    setTimeout(() => {
+      resolved(user);
+    }, 3000);
+  }
+});
+
+myPromise
+  .then((res) => console.log(res))
+  .catch((err) => {
+    // console.table({err, name: err.name, message: err.message, stack: err.stack });
+  });
+
+let useData = [];
+const userId = [10, 20, 30, 40, 50, 60];
+
+for (let i = 0; i < userId.length; i++) {
+  const userIDs = userId[i];
+  // console.log(userIDs)
+  useData.push(myPromise);
+}
+// console.log(useData,"useData");
+
+Promise.all(useData).then((res) => {
+  console.log(res);
+});
